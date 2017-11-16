d265671b158af178e6579394b273345009e4f077
renshanshan <15734033207@163.com>
2017-11-16 13:00:26 +0800

    方案二插件翻译完善

--- webclient/trans.js
+++ webclient/trans.js
@@ -8,9 +8,9 @@ function translate(arg){
         data:{t:s},
         success:function(data){
             if(data == null){
-                jQuery(arg).text("待翻译");
+                jQuery(arg).html("待翻译");
             }else{
-                jQuery(arg).text(data);  
+                jQuery(arg).html(data);  
             }                            
         }
     });
@@ -32,12 +32,56 @@ function transButton(arg){
         }
     });
 }
+
+function buttonExcute(arg){
+	jQuery(arg).each(function(){
+		translate(this);
+	});
+}
 i2b2.events.afterLogin.subscribe(function(){
     //default.htm页面
-    var en = "#ontFindCategory option,.temporalControl a,.temporalControl a,#temporalbuilder_0 option,#addDefineGroup-button,#groupCount span,#queryBalloonAnd1,#queryBalloonAnd2,#queryBalloon1,#queryPanelTitle1,#queryPanelTitle2,#queryPanelTitle3,.qryButtonOccurs .occurs,.queryLabel,.yui-nav li a,#anaPluginView option,#menubutton1select option,.qryButtonExclude a,.qryButtonDate a,#newBox a,#ontFormFindName option,.tabBox div,#topBar a:lt(2),#topBar a:gt(2),#ontFindDisp a,#crcFindCategory option,#menubutton2select option,#crcFindStrategy option,.initialMsg a,#crcFindButton a,#PluginListBox span a:eq(2),.topmenu span,#tabNavigate div,#tabFind div,#runBoxText,#addDefineGroup,#removeDefineGroup,#ontFormFindName table option";
-    jQuery(en).each(function(){
+    jQuery("#queryBalloonBox .queryBalloon,#optionsHistory span,#optionsQT span,#optionsOntNav span,#optionsOntFind span,#ontFindCoding option,#ontFindCategory option,.temporalControl a,.temporalControl a,#temporalbuilder_0 option,#addDefineGroup-button,#groupCount span,#queryBalloonAnd1,#queryBalloonAnd2,#queryBalloon1,#queryPanelTitle1,#queryPanelTitle2,#queryPanelTitle3,.qryButtonOccurs .occurs,.queryLabel,.yui-nav li a,#anaPluginView option,#menubutton1select option,.qryButtonExclude a,.qryButtonDate a,#newBox a,#ontFormFindName option,.tabBox div,#topBar a:lt(2),#topBar a:gt(2),#ontFindDisp a,#crcFindCategory option,#menubutton2select option,#crcFindStrategy option,.initialMsg a,#crcFindButton a,#PluginListBox span a:eq(2),.topmenu span,#tabNavigate div,#tabFind div,#runBoxText,#addDefineGroup,#removeDefineGroup,#ontFormFindName table option").each(function(){
         translate(this);
     });
+
+    //show options确认取消
+    var flag = true;
+    jQuery("#ontMainBox .opXML").delegate("[title='Show Options']","click",function(){
+		if(flag){
+    		setTimeout("buttonExcute('.yui-panel-container .ft button')",0.1);
+    		flag = false;
+    	}
+    });
+  //   jQuery("#ontMainBox .opXML").delegate("[title='Show Options']","click",function(){
+		// if(flag){
+  //   		setTimeout("buttonExcute('#optionsOntFind .yui-panel-container .ft button')",0.1);
+  //   		let flag = false;
+  //   	}
+  //   });
+  //   jQuery("#crcHistoryBox .opXML").delegate("[title='Show Options']","click",function(){
+		// if(flag){
+  //   		setTimeout("buttonExcute('#optionsHistory .ft button')",0.1);
+  //   		let flag = false;
+  //   	}
+  //   });
+  //   jQuery("#crcHistoryBox .opXML").delegate("[title='Show Options']","click",function(){
+		// if(flag){
+  //   		setTimeout("buttonExcute('#optionsHistory .yui-panel-container .ft button')",0.1);
+  //   		let flag = false;
+  //   	}
+  //   });
+    // jQuery("#crcHistoryBox").delegate("a:eq(2)","click",function(){
+    // 	if(flag){
+    // 		setTimeout("buttonExcute()",0.1);
+    // 		flag = false;
+    // 	}
+    // });
+    // jQuery("#crcQueryToolBox").delegate("a:eq(1)","click",function(){
+    // 	if(flag){
+    // 		setTimeout("buttonExcute()",0.1);
+    // 		flag = false;
+    // 	}
+    // });
 
     //改变密码弹出框
     jQuery(".hd,#modifier-viewer-body tr td:even()").each(function(){
@@ -48,6 +92,13 @@ i2b2.events.afterLogin.subscribe(function(){
         transButton(this);
     });
 
+    //点击添加时间关系
+    jQuery("center").degelate(".temporalControl","click",function(){
+    	alert("11");
+    	jQuery(".relationshipAmongEvents:gt(1) option").each(function(){
+    		translate(this);
+    	});
+    });
     //点击清空按钮
     // jQuery(".qryPanelClear a").click(function(){
     //     jQuery(".yuimenuitem a,#queryTiming-button,#queryPanelTitle1,#queryPanelTitle2,#queryPanelTitle3").each(function(){
@@ -75,10 +126,20 @@ i2b2.events.afterLogin.subscribe(function(){
     });
 
     jQuery("#anaPluginList").on("click",function(){
-        jQuery("#anaPluginViewBox .tabBox div,.yui-nav em").each(function(){
+        jQuery("#anaPluginViewBox .tabBox div,.yui-nav em,.Dem1Set-MainContentPad div,.results-directions,.Dem1Set-MainContentPad h1,.Dem1Set-MainContentPad h2,.Dem1Set-MainContentPad p,.Dem2Set-MainContentPad div,.Dem2Set-MainContentPad h1,.Dem2Set-MainContentPad h2,.Dem2Set-MainContentPad p,.Timeline-MainContentPad div:lt(4),.Timeline-MainContentPad .concptItem,.Timeline-MainContentPad #Timeline-DeleteMsg,.Timeline-MainContentPad h1,.Timeline-MainContentPad h2,.Timeline-MainContentPad p,.WISEsearcher-MainContentPad div:eq(0),#WISEsearcher-caseOptions b,#WISEsearcher-caseOptions li,#WISEsearcher-matchOptions b,#WISEsearcher-excludeOptions b").each(function(){
             translate(this);
         });
+        jQuery(".WISEsearcher-MainContentPad span,.WISEsearcher-MainContentPad h1,.WISEsearcher-MainContentPad li,.WISEsearcher-MainContentPad table td,.CAREcncptDem-MainContentPad div:lt(4),.CAREcncptDem-MainContentPad .droptrgtlbl,.CAREcncptDem-MainContentPad .concptItem").each(function(){
+            translate(this);
+        });
+        jQuery("#CAREcncptDem-ConceptHint i,.CAREcncptDem-MainContent .export-directions,.CAREcncptDem-MainContentPad h1,.CAREcncptDem-MainContentPad span,.CAREcncptDem-MainContentPad li,.CAREcncptDem-MainContentPad td,.CAREobsTally-MainContentPad div:lt(4),.CAREobsTally-MainContentPad .droptrgtlbl,.CAREobsTally-MainContentPad .concptItem,#CAREobsTally-ConceptHint i,.CAREobsTally-MainContentPad span,.CAREobsTally-MainContentPad li,#CAREobsTally-Main span,.CAREobsTally-MainContentPad h1,.CAREobsTally-MainContentPad td").each(function(){
+            translate(this);
+        });
+        jQuery(".ExportXLS-MainContentPad div:lt(4),.ExportXLS-MainContentPad .droptrgtlbl,.ExportXLS-MainContentPad .concptItem,#ExportXLS-ConceptHint i,.optionitems b,.optionitems option,.optionitems label,.ExportXLS-MainContentPad h1,.ExportXLS-MainContentPad span,.ExportXLS-MainContentPad ul li,.ExportXLS-MainContentPad li span,.ExportXLS-MainContentPad:eq(2) td,.ExportXLS-MainContentPad ol li span,.ExportXLS-MainContentPad ol li:gt(7)").each(function(){
+        	translate(this);
+        });
     });
+
 
     jQuery(".initialMsg a").on("click",function(){
         jQuery(".txtBoundBox div,#anaPluginCats option").each(function(){
@@ -150,7 +211,11 @@ jQuery(document).ready(function(){
     jQuery("#menubutton2select option,#addDefineGroup,#removeDefineGroup").each(function(){
         translate(this);
     });
+
+   
 });
+
+
 
 
 
