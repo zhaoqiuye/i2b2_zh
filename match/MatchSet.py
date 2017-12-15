b6441764bca2cbe6f301b9c6abea7ccb69d68467
caicai <15734080618@163.com>
2017-12-15 16:41:53 +0800

    测试通过版本

--- match/MatchSet.py
+++ match/MatchSet.py
@@ -6,13 +6,13 @@ from sqlalchemy.orm import sessionmaker
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String
 
-enginepsqli2b2 = create_engine("postgresql://postgres:postgres@59.110.164.110:5432/i2b2metadata", max_overflow=5,
+enginepsqli2b2 = create_engine("postgresql://postgres:postgres@localhost:5432/i2b2metadata", max_overflow=5,
                                encoding='utf8', echo=True)
-enginepsqlChinese = create_engine("postgresql://postgres:postgres@59.110.164.110:5432/medicine", max_overflow=5,
-                             encoding='utf8',
-                             echo=True)
+enginepsqlChinese = create_engine("postgresql://postgres:postgres@localhost:5432/medicine", max_overflow=5,
+                                  encoding='utf8',
+                                  echo=True)
 Base = declarative_base()
 
 
@@ -26,21 +26,15 @@ class Mecidine(Base):
         self.c_fullname = c_fullname
         self.c_name = c_name
 
-
 class ChinesePharma(Base):
     __tablename__ = 'ChinesePharma'
-    name = Column(String(255), primary_key=True)
-    type = Column(String(50))
-
-    def __init__(cls, name, type):
-        self = cls
-        self.name = name
-        self.type = type
+    m_name = Column(String(255), primary_key=True)
+    type = Column(String(255))
 
 
 def queryMeicData():
     '''
-    查询数据
+    查询metatdata数据
     :rtype: object
     :return:
     '''
@@ -51,6 +45,7 @@ def queryMeicData():
     qlist = []
     for qs in alist:
         qlist.append(qs.c_name)
+        print str(qs.c_name)
     session.commit()
     session.close()
     return qlist
@@ -58,23 +53,18 @@ def queryMeicData():
 
 def queryChineseData():
     '''
-    查询数据
+    查询中国药典数据
     :rtype: object
     :return:
     '''
     print "query start ...."
     DBSession = sessionmaker(bind=enginepsqlChinese)
     session = DBSession()
-    alist = session.query(Mecidine).filter(Mecidine.c_fullname.like('%Medications%')).all()
+    alist = session.query(ChinesePharma).all()
     qlist = []
     for qs in alist:
-        qlist.append(qs.c_name)
+        qlist.append(qs.m_name)
+        print qs.m_name
     session.commit()
     session.close()
     return qlist
-
-
-if __name__ == '__main__':
-    result = queryMeicData()
-    for i in result:
-        print i
--- match/check_ontology.py
+++ match/check_ontology.py
@@ -1,34 +1,39 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import logging
-logging.basicConfig(filename='check.log', filemode='w', level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s') 
-from fuzzywuzzy import fuzz, process
 
-if 1: 
+logging.basicConfig(filename='check.log', filemode='w', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s')
+from fuzzywuzzy import fuzz, process
+from MatchSet import queryChineseData, queryMeicData
+
+if 1:
     """
     for inline debugging
     """
-    test_refSet=set([u"数据分析师", u"数据挖掘工程师", u"大数据开发工程师", u"机器学习工程师", u"算法工程师", u"数据库管理", u"商业分析师", u"数据科学家", u"首席数据官", u"数据产品经理", u"数据运营", u"大数据架构师"]) 
-    testSet = set([u"算法工程师", u"数据库管理", u"商业分析师", u"数据挖掘师"])
+    # test_refSet=set([u"数据分析师", u"数据挖掘工程师", u"大数据开发工程师", u"机器学习工程师", u"算法工程师", u"数据库管理", u"商业分析师", u"数据科学家", u"首席数据官", u"数据产品经理", u"数据运营", u"大数据架构师"])
+    # testSet = set([u"算法工程师", u"数据库管理", u"商业分析师", u"数据挖掘师"])
+    test_refSet = set(queryChineseData())
+    testSet = set(queryMeicData())
+
 
 class RefSet(object):
-    def __init__(self, dataSet=test_refSet): 
-        self.dataSet = dataSet 
-        logging.info('There are totally %d elements in the reference set'%len(self.dataSet))
+    def __init__(self, dataSet=test_refSet):
+        self.dataSet = dataSet
+        logging.info('There are totally %d elements in the reference set' % len(self.dataSet))
 
     def intersect(self, otherSet):
         s = self.dataSet.intersection(otherSet)
-        logging.info('There are %d elements in both sets:'%len(s))
+        logging.info('There are %d elements in both sets:' % len(s))
         for a in sorted(s): print a,
         print
         return s
 
-    def fuzzyMatch(self, otherSet): 
+    def fuzzyMatch(self, otherSet):
         """
         return a dictionary: mapping every item in otherSet to its best match in the reference set.
         """
-        return dict([ (item,  process.extractOne(item, self.dataSet, scorer=fuzz.ratio)) 
-            for item in otherSet ])
+        return dict([(item, process.extractOne(item, self.dataSet, scorer=fuzz.ratio))
+                     for item in otherSet])
 
 
 def exp1():
@@ -48,12 +53,8 @@ def exp2():
     for key, item in matchDict.items():
         print key, ':', item[0], item[1]
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import sys
-    exec '%s()'%sys.argv[1]
-        
 
-
-
-        
-
+    exec '%s()' % sys.argv[1]
